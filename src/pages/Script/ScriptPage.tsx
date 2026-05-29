4151013dbaddc82105b33da53912555828756da7
Павел <pavel.polyakoff@gmail.com>
2026-05-29 15:24:09 +0300

    Fix: кнопка Создать с нуля сразу ведет на CreateScriptPage без модального окна

--- src/pages/Script/ScriptPage.tsx
+++ src/pages/Script/ScriptPage.tsx
@@ -201,7 +201,7 @@ export default function ScriptPage() {
 
             {/* Написать с нуля */}
             <button
-              onClick={() => setShowFormatModal(true)}
+              onClick={() => navigate(`/project/${project?.id}/script/create`)}
               onMouseEnter={() => setCreateHover(true)}
               onMouseLeave={() => setCreateHover(false)}
               className="relative flex flex-col items-start rounded-2xl p-6 text-left transition-all overflow-hidden"
